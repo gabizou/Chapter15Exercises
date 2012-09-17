55fc8644fae00d2f9ca4e4cf1cc08d1a46b57c33
Gabriel Harris-Rouquette <gabizou@me.com>
2012-09-16 18:52:54 -0700

    Fixed redundant imports for JUnit Testing.

--- 15.2/test/com/gabizou/TriangleTest.java
+++ 15.2/test/com/gabizou/TriangleTest.java
@@ -1,7 +1,6 @@
 package com.gabizou;
 
-import static org.junit.Assert.*;
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -9,12 +8,12 @@ public class TriangleTest {
 
 	@Test
 	public void test() {
-		GeometricObject tester = new Triangle(3, 4, 5, "white", true);
-		Assert.assertEquals(true, tester.isFilled());
-		Assert.assertEquals("The color doesn't match!", "white", tester.getColor());
-		Assert.assertEquals("The height isn't working!", 4.0, 4.0, 0.01);
-		Assert.assertEquals("The area ins't being calculated right!", 6.0, tester.getArea(), 0.01);
-		Assert.assertEquals("The perimeter isn't being calculated right!", 12.0, tester.getPerimeter(), 0.01);
+		GeometricObject tester = new Triangle(3.0, 4.0, 5.0, "white", true);
+		assertEquals(true, tester.isFilled());
+		assertEquals("The color doesn't match!", "white", tester.getColor());
+		assertEquals("The height isn't working!", 4.0, 4.0, 0.01);
+		assertEquals("The area ins't being calculated right!", 6.0, tester.getArea(), 0.01);		
+		assertEquals("The perimeter isn't being calculated right!", 12.0, tester.getPerimeter(), 0.01);
 	}
 
 }
